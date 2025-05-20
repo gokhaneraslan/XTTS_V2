be56e3250a937b112a97f3e72210f2f84264eca8
gokhaneraslan <92183319+gokhaneraslan@users.noreply.github.com>
2025-05-20 12:11:32 +0300

    Update lora_inference.py

--- lora_inference.py
+++ lora_inference.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import torch
 import torchaudio
 from pathlib import Path
@@ -27,7 +28,8 @@ XTTS_LORA_ORIGINAL_CONFIG_PATH = LORA_ADAPTER_PATH / "original_xtts_config.json"
 
 
 SPEAKER_REFERENCE_WAV_PATH = SPEAKER_REFERENCE_ROOT / "reference.wav"
-OUTPUT_WAV_PATH = OUTPUT_ROOT_DIR / "samples" / "output.wav"
+OUTPUT_PATH = OUTPUT_ROOT_DIR / "samples"
+OUTPUT_WAV_PATH = OUTPUT_ROOT_DIR / "output.wav"
 
 
 def check_input_files_exist():
@@ -112,7 +114,8 @@ def main(text:str, language: str):
         temperature=0.75,
     )
 
-    torchaudio.save(OUTPUT_WAV_PATH, torch.tensor(out["wav"]).unsqueeze(0), 24000)
+    os.makedirs(OUTPUT_PATH, exist_ok=True)
+    torchaudio.save(str(OUTPUT_WAV_PATH), torch.tensor(out["wav"]).unsqueeze(0), 24000)
     print(f"Output audio file '{OUTPUT_WAV_PATH}' saved")
 
 
